eb3e9b4a4b19d21549c7cabbfd641edb7eaf3cf3
Vu Thang <dangvuthang97@gmail.com>
2020-05-22 08:47:54 +0700

    Initial test data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users(email, name, password) VALUES('sirachilles83@gmail.com', 'Phuc', '123');
+INSERT INTO users(email, name, password) VALUES('Vu.3784@gmail.com', 'Vu', '123');
+INSERT INTO users(email, name, password) VALUES('jun154607@gmail.com', 'Giang', '123');
+
+
+
