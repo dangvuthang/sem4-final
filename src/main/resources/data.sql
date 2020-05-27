7b304947a1e10a59791c466b0503a014e7f78a46
Vu Thang <dangvuthang97@gmail.com>
2020-05-27 19:12:31 +0700

    Add test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO users(email, name, password) VALUES('user1@gmail.com', 'Phuc', '123');
-INSERT INTO users(email, name, password) VALUES('user2@gmail.com', 'Vu', '123');
-INSERT INTO users(email, name, password) VALUES('user3@gmail.com', 'Giang', '123');
-
-
-
+INSERT INTO roles(name) VALUES('admin'), ('user'), ('guide');
+INSERT INTO users(email, password, phone, name, role_id)
+VALUES ('user1@gmail.com', '123', '0771123456', 'User1', 2),
+              ('user2@gmail.com', '123', '0772233489', 'User2', 2),
+              ('user3@gmail.com', '123', '0773346745', 'User3', 2)
+               
